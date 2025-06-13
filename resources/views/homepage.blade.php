806558cc8a4d4737690b3ad55374b5e7091ac0f6
ketace06 <ismail.omaramin06@gmail.com>
2025-06-13 16:04:39 +0200

    feat: update dashboard and setup a layout for sidebar

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,12 +1,10 @@
 @extends('layout')
-
 @section('title', 'Dashboard')
 @section('content')
 <header class="w-2/3">
     <h1 class="text-3xl font-bold">Dashboard</h1>
     <hr class="border-t border-gray-300 w-full mt-5">
 </header>
-
 <main class="mt-10">
     <div class="mt-5">
         <label for="formation" class="mr-2 font-medium">Formation :</label>
@@ -18,11 +16,10 @@
     <div class="mt-10 mb-6">
         <input type="text" placeholder="Rechercher un apprenti..." class="border rounded px-3 py-2 w-80">
     </div>
-
-    @foreach (range(1, 6) as $i)
+    @foreach (range(1, 8) as $i)
     <div class="flex flex-col items-start space-y-6 mb-4">
         <div class="flex items-center space-x-4 bg-white p-4 rounded-lg">
-            <img src="votre-image.jpg" alt="Photo de l'apprenti" class="w-16 h-16 rounded-full border-2" style="border-color: var(--color-green-50)">
+            <img src="" alt="Photo" class="w-16 h-16 rounded-full border-2" style="border-color: var(--color-green-50)">
             <div>
                 <h3 class="text-2xl font-semibold text-gray-800">Nom de l'apprenti</h3>
                 <p class="text-sm text-gray-600">Formation : {{ $i % 2 == 0 ? 'Employé·e de commerce' : 'Informaticien·ne en développement d\'applications' }}</p>
