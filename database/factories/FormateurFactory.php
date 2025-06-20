76e2808db29ba983fba2f1aa57cc209595ef8540
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-06-20 11:24:28 +0200

    feat: created factory for each type of user and created seeder (#88)

---
+++ database/factories/FormateurFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Formateur>
+ */
+class FormateurFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'lastname' =>   fake()->lastName(),
+            'role' => fake()->randomElement(['Formateur-Commerce',  'Formateur-Informaticien']),
+            'email' =>  fake()->email(),
+            'password'  =>  fake()->password(),
+        ];
+    }
+}
