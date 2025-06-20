e991f1c42f180b69a256d82909fa24e737100b25
ketace06 <ismail.omaramin06@gmail.com>
2025-06-20 14:58:21 +0200

    feat: finish static description of entreprise

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,9 @@ Route::get('/login', function () {
     return view('login');
 });
 
-
 Route::get('/createcomments', function () {
     return view('createcomments');
 });
-
 
 Route::get('/', function () {
     return view('homepage');
