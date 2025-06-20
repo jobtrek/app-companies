daa1809cee3bc9c3ed79b29e1db604861aad22e0
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-06-20 11:20:50 +0200

    feat: created factory for each type of user and created seeder

--- app/Models/Apprenti.php
+++ app/Models/Apprenti.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Apprenti extends User
 {
+    use HasFactory;
     protected $fillable = [
         'name',
         'lastname',
