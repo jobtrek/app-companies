95b2d2bb03757e56895e2ac02987385b292d0444
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-06-20 09:35:09 +0200

    feat: displayed all apprentis on home page

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,13 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function() {
-    return view('homepage');
-});
-
-
 Route::get('/login', function () {
     return view('login');
 });
 
-Route::post('/creation', [UserController::class, 'create'])->name('creation');
-
 Route::post('/login', [UserController::class, 'login'])->name('login');
 
+Route::post('/creation', [UserController::class, 'create'])->name('creation');
+
+
+Route::get('/', [UserController::class, 'index'])->name('home');
