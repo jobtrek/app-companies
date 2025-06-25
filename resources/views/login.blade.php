eb4278e081241113c390e598b8e5ac0274cbbc89
maryam-amn <Maryam.Aman@jobtrek.ch>
2025-06-25 10:11:04 +0200

    feat: creation of the page to create a new account

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,7 +12,7 @@
     <div class="w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl relative rounded-lg px-4">
 
         <img
-            src="{{ asset('images/Job.png') }}"
+            src="{{ asset('images/Jobtrek_logo.png') }}"
             class="bg-white rounded-full w-24 h-24 object-cover absolute -top-12 left-1/2
                    transform -translate-x-1/2 border-4 shadow-lg border-regal-blue"
         />
@@ -21,16 +21,10 @@
 
             <div class="flex flex-col gap-6 mt-4">
                 <h1 class="font-[Roboto_Mono] text-2xl sm:text-3xl text-center text-white">Connexion</h1>
-                <select class="text-white border border-white text-center rounded-lg bg-transparent p-2">
-                    <option>Se connecter en tant que ...</option>
-                    <option class="text-black" value="Apprenti">Apprenti</option>
-                    <option class="text-black" value="Formateur Informaticien">Formateur Informaticien</option>
-                    <option class="text-black" value="Formateur Employé de commerce">Formateur Employé de commerce</option>
-                    <option class="text-black" value="Coach">Coach</option>
-                </select>
+
                 <input type="text"
                        placeholder="Email"
-                       class="text-white border p-2 rounded-lg bg-transparent placeholder-gray-300 text-center"/>
+                       class="text-white border p-2 rounded-lg  placeholder-gray-300 text-center "/>
                 <input type="password"
                        placeholder="Mot de passe"
                        class="text-white border p-2 rounded-lg bg-transparent placeholder-gray-300 text-center"/>
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,20 @@
 
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+Route::get('/', function () {
+    return view('homepage');
+});
 
 Route::get('/login', function () {
     return view('login');
 });
 
-
 Route::get('/createcomments', function () {
     return view('createcomments');
 });
 
-
-Route::get('/', function () {
-    return view('homepage');
+Route::get('/createaccount', function () {
+    return view('createaccount');
 });
 
 
