7586535188904276631c3e130466c0f4253e6305
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-06-27 09:43:16 +0200

    feat: add filtering options and dynamic sorting for "Apprentis" on the homepage

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,19 +36,24 @@ class UserController extends Controller
 
     public function index()
     {
-        $apprenties = User::whereJsonContains('roles', 'coach')->get();
+        $apprenties = User::whereJsonContains('roles', 'apprenti_informaticien')->orWhereJsonContains('roles', 'apprenti_commerce')->get();
 
-        return view('homepage', ['apprentis' => $apprenties]);
+        $filtres = [
+            'entreprise' => 'En entreprise',
+            'formation' => 'En centre de formation',
+            'alphabetique' => 'Par ordre alphabétique',
+            'informaticien-dev' => 'Informaticien développement',
+            'employe-com' => 'Employé de commerce',
+        ];
+
+        return view('homepage', ['apprentis' => $apprenties, 'filtres' => $filtres]);
     }
 
     public function coach()
     {
-        $users = User::all();
-        $apprenties = $users->filter(function ($user) {
-            return $user->roles->contains('coach');
-        });
+        $apprentis = User::all()->whereNotNull('coach_id');
 
-        return view('coach', ['apprentis' => $apprenties]);
+        return view('coach', ['apprentis' => $apprentis]);
     }
 
     public function show($id)
@@ -56,5 +61,34 @@ class UserController extends Controller
         $user = User::findOrFail($id);
 
         return view('userProfile', ['user' => $user]);
+    }
+
+    public function sort($sort)
+    {
+        $filtres = [
+            'entreprise' => 'En entreprise',
+            'formation' => 'En centre de formation',
+            'alphabetique' => 'Par ordre alphabétique',
+            'informaticien-dev' => 'Informaticien développement',
+            'employe-com' => 'Employé de commerce',
+        ];
+
+        $apprenties = [];
+        if($sort == 'entreprise') {
+            $apprenties = User::all()->whereNotIn('enterprise', ['Centre de formation Jobtrek']);
+        } else if($sort == 'formation') {
+            $apprenties = User::all()->where('enterprise', 'Centre de formation Jobtrek');
+        } else if($sort == 'alphabetique') {
+            $apprenties = User::all()->sortBy('name');
+        } else if($sort == 'informaticien-dev') {
+            $apprenties = User::whereJsonContains('roles', 'apprenti_informaticien')->get();
+        } else if($sort == 'employe-com') {
+            $apprenties = User::whereJsonContains('roles', 'apprenti_commerce')->get();
+        } else {
+            return back()->withErrors(['Choisissez un filtre valable.']);
+        }
+
+
+        return view('homepage', ['apprentis' => $apprenties, 'filtres' => $filtres]);
     }
 }
