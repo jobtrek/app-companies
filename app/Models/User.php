b7b075c40ad7cf795b12647bdf68e50d6be83c46
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-07-02 13:34:38 +0200

    feat: added domain table with model relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,9 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Casts\AsCollection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -65,21 +68,25 @@ class User extends Authenticatable
 
     public function file(): MorphOne
     {
-
         return $this->morphOne(File::class, 'fileable');
     }
 
-    public function coach()
+    public function coach(): BelongsTo
     {
         return $this->belongsTo(User::class, 'coach_id');
     }
 
-    public function apprentis()
+    public function apprentis(): HasMany
     {
         return $this->hasMany(User::class, 'coach_id');
     }
-    public function commentaires()
+    public function commentaires(): HasMany
     {
         return $this->hasMany(Commentaire::class, 'apprentis_id');
+    }
+
+    public function domain(): BelongsTo
+    {
+        return $this->belongsTo(Domain::class);
     }
 }
