5c4a3fd8c283e99ac0690d367301307ecc51abd6
maryam-amn <Maryam.Aman@jobtrek.ch>
2025-07-02 10:53:33 +0200

    feat: the user can add comments

--- resources/views/createcomments.blade.php
+++ resources/views/createcomments.blade.php
@@ -14,26 +14,46 @@
     </script>
 @endsection
 @section('content')
-    <h1 class="text-2xl font-bold mb-6">Commentaire sur Apprenti (x)</h1>
-    <form class="grid grid-cols-2 md:grid-cols-2 gap-5 min-h-[80vh]">
-        <div class="flex flex-col gap-4 h-full">
-            <input type="text" class="bg-gray-600 h-10 w-full rounded px-3 text-white"
-                placeholder="Veuillez écrire le titre de votre commentaire">
-            <textarea class="bg-blue-100 w-full flex-grow p-3 rounded resize-none" placeholder="Votre commentaire"></textarea>
+
+    <h1 class="text-2xl font-bold mb-6">Commentaire sur l' apprenti : {{$user->name}} </h1>
+    @if ($errors->any())
+        <div class="mb-4 p-4 bg-red-100 text-red-700 rounded">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
-
+    @endif
+    <form method="POST" action="{{ route('create.comment', ['id' => $user->id]) }}"
+          class="grid grid-cols-2 md:grid-cols-2 gap-5 min-h-[80vh]">
+        @csrf
+        <div class="flex flex-col gap-4 h-full">
+            <input type="text" class="bg-gray-600 h-10 w-full rounded px-3 text-white" name="title"
+                   placeholder="Veuillez écrire le titre de votre commentaire">
+            <textarea name="description" class="bg-blue-100 w-full flex-grow p-3 rounded resize-none"
+                      placeholder="Votre commentaire"></textarea>
+        </div>
+        <input type="hidden" name="apprentis_id" value="{{$user->id}}">
         <div class="flex flex-col gap-4 md:items-start">
-            <img id="preview" class="bg-blue-100 w-full md:w-5/6 h-48 md:h-full p-4 rounded object-contain" />
+            <img id="preview" class="bg-blue-100 w-full md:w-5/6 h-48 md:h-full p-4 rounded object-contain"/>
 
-            <div class="flex flex-row gap-4 items-center w-full max-w-md">
+            <div class="flex flex-row gap-4 justify-center items-center w-full max-w-md">
                 <input type="file" id="fileInput" style="display: none;" onchange="showFileName()">
                 <button type="button"
-                    class="bg-[#1E1D1C] hover:bg-[#1C2366] active:scale-95 active:bg-[#101426] text-white px-4 py-2 transition-all duration-200 ease-in-out rounded"
-                    onclick="document.querySelector('#fileInput').click();">Choisir un fichier
+                        class="bg-[#1E1D1C] hover:bg-[#1C2366] active:scale-95 active:bg-[#101426] text-white px-4 py-2 transition-all duration-200 ease-in-out rounded"
+                        onclick="document.querySelector('#fileInput').click();">Choisir un fichier
                 </button>
-                <p id="fileName" class="bg-blue-100 px-4 py-2 mt-2 text-sm text-blue-700 truncate flex-grow">Aucun fichier
+                <p id="fileName" class="bg-blue-100 px-4 py-2 mt-2 text-sm text-blue-700 truncate flex-grow">Aucun
+                    fichier
                     choisi</p>
+                <button type="submit"
+                        class="bg-[#1E1D1C] hover:bg-green-700 active:scale-95 active:bg-[#101426] text-white px-4 py-2 transition-all duration-200 ease-in-out rounded">
+                    Submit
+                </button>
             </div>
+
         </div>
+
     </form>
 @endsection
