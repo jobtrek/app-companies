5aa429f130f44c8f1386c58bb6994639853c7852
maryam-amn <Maryam.Aman@jobtrek.ch>
2025-07-03 13:49:40 +0200

    fix: all the code that handle events are in another file

--- resources/views/userProfile.blade.php
+++ resources/views/userProfile.blade.php
@@ -1,33 +1,5 @@
 @extends('layout')
-@section('script')
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            const toggleBtn = document.getElementById('toggleButton');
-            const dropdown = document.getElementById('dropdownWrapper');
-
-            toggleBtn.addEventListener('click', function () {
-                dropdown.classList.toggle('hidden');
-            });
-        });
-        const toggleButton = document.getElementById('toggleButton');
-        const dropdownWrapper = document.getElementById('dropdownWrapper');
-        const submitBtn = document.getElementById('submitBtn');
-        const selectCoach = document.getElementById('coach_id');
-
-
-        selectCoach.addEventListener('change', () => {
-            if (selectCoach.value) {
-                submitBtn.classList.remove('hidden');
-            } else {
-                submitBtn.classList.add('hidden');
-            }
-        });
-
-
-    </script>
-@endsection
 @section('title', 'Profil utilisateur')
-
 @section('content')
     <div class="max-w-4xl mx-auto bg-white rounded-xl shadow-lg mt-15 sm:flex sm:flex-row p-8 gap-8">
 
