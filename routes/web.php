ceee2e68ea5a537a717f16a94d9f35a538364921
Ismail <ismail.omaramin06@gmail.com>
2025-07-03 12:10:12 +0200

    feat: implement company creation with domain validation and store company in database (tested with single domain) (#143)

--- routes/web.php
+++ routes/web.php
@@ -5,44 +5,23 @@ use App\Http\Controllers\EntrepriseController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/', [UserController::class, 'index'])->name('home');
 
-Route::get('/create-comment', function () {
-    return view('createcomments');
-});
-
-Route::get('/company-profile', function () {
-    return view('profileEntreprise');
-});
-Route::get('/coach', function () {
-    return view('coach');
-})->name('coach');
-
-Route::get('/create-account', function () {
-    return view('createaccount');
-})->name('create-account');
-
-Route::get('/create-company', function () {
-    return view('createCompany');
-})->name('create-company');
+Route::view('/login', 'login')->name('login.view');
+Route::view('/create-account', 'createaccount')->name('create-account');
+Route::view('/company-profile', 'profileEntreprise')->name('company-profile');
 
 Route::post('/login', [UserController::class, 'login'])->name('login');
-
 Route::post('/creation', [UserController::class, 'create'])->name('creation');
 
 Route::get('/profileEntreprise/{entreprise}', [EntrepriseController::class, 'index'])->name('profileEntreprise');
-Route::post('create-new-company', [EntrepriseController::class, 'create'])->name('company.create');
+Route::get('/create-company', [EntrepriseController::class, 'showCreateForm'])->name('create-company');
+Route::post('/create-new-company', [EntrepriseController::class, 'create'])->name('company.create');
 
 Route::get('/user-profile/{id}', [UserController::class, 'show'])->name('userProfile');
 
-Route::get('/', [UserController::class, 'index'])->name('home');
-
 Route::get('/coach', [UserController::class, 'coach'])->name('coach');
-
-Route::get('sort/{sort}', [UserController::class, 'sort'])->name('home.sort');
-
+Route::get('/sort/{sort}', [UserController::class, 'sort'])->name('home.sort');
 Route::get('/comments-detail/{id}', [UserController::class, 'commentsdetails'])->name('comment.detail');
 
 Route::get('/create-comment/{id}', [CommentController::class, 'commentview'])->name('create.comment.view');
