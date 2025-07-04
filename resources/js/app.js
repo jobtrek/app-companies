e44c6421e68cd1fc64334bdae1f6f82f2a6c5b1c
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-07-04 11:15:16 +0200

    feat: add entreprise linking functionality in user profile

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import "./bootstrap";
 
+//Change coach
 document.addEventListener('DOMContentLoaded', function () {
     const toggleBtn = document.getElementById('toggleButton');
     const dropdown = document.getElementById('dropdownWrapper');
@@ -21,6 +22,29 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+//Change entreprise
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleBtnEntreprise = document.getElementById('toggleButtonEntreprise');
+    const dropdownEntreprise = document.getElementById('dropdownWrapperEntreprise');
+    if (toggleBtnEntreprise && dropdownEntreprise) {
+        toggleBtnEntreprise.addEventListener('click', function () {
+            dropdownEntreprise.classList.toggle('hidden');
+        });
+    }
+    const submitBtnEntreprise = document.getElementById('submitBtnEntreprise');
+    const selectEntreprise = document.getElementById('entreprise_id');
+    if (selectEntreprise && submitBtnEntreprise) {
+        selectEntreprise.addEventListener('change', () => {
+            if (selectEntreprise.value) {
+                submitBtnEntreprise.classList.remove('hidden');
+            } else {
+                submitBtnEntreprise.classList.add('hidden');
+            }
+        });
+    }
+});
+
+//Change app to entr at homepage
 document.addEventListener("DOMContentLoaded", function () {
     const btnApprentis = document.getElementById("btn-apprentis");
     const btnEntreprises = document.getElementById("btn-entreprises");
@@ -69,6 +93,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+//Search
 document.addEventListener('DOMContentLoaded', function() {
     const searchInput = document.querySelector('#search');
 
