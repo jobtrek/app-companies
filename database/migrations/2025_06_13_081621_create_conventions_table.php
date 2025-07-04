5a87081e869acb14fd69307a2145616c03c4e860
ketace06 <ismail.omaramin06@gmail.com>
2025-07-04 15:25:51 +0200

    feat: remove cascade

--- database/migrations/2025_06_13_081621_create_conventions_table.php
+++ database/migrations/2025_06_13_081621_create_conventions_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->foreignId('users_id')->constrained();
-            $table->foreignId('entreprise_id')->constrained()->onDelete('cascade');
+            $table->foreignId('entreprise_id')->constrained();
             $table->date('date_de_départ');
             $table->date('date_de_retour');
 
