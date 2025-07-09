54d16f5a0ec01c869aff19db3b0ea1ce8e77c319
Maryam <meryem.2005.aman@gmail.com>
2025-07-09 13:18:54 +0200

    feat: we can delete, add or update a comments (#178)

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CommentCreationRequest;
 use App\Models\Commentaire;
 use App\Models\User;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class CommentController extends Controller
 {
+    use AuthorizesRequests;
+
     //
     public function comment(CommentCreationRequest $request, $id)
     {
@@ -23,6 +26,10 @@ class CommentController extends Controller
 
     public function commentview($id)
     {
+        $user = auth()->user();
+        if (!$user->roles->contains('coach')) {
+            return redirect()->route('coach')->with('error', "Vous n'avez pas les accés nécesssaire pour l'ajout de commentaire.");
+        }
         $user = User::findOrFail($id);
 
         return view('createcomments', ['user' => $user]);
@@ -30,10 +37,48 @@ class CommentController extends Controller
 
     public function commentsdetails($id)
     {
+        $user = auth()->user();
+        if (!$user->roles->contains('coach')) {
+            return redirect()->route('coach')->with('error', "Vous n'avez pas les accés pour voir les commentaires.");
+        }
         $commentaire = Commentaire::all()->find($id);
 
         $user = User::all()->where('id', $commentaire->apprentis_id)->firstOrFail();
 
         return view('commentDetails', ['comments' => $commentaire, 'user' => $user]);
     }
+
+    public function destroyComment(Commentaire $commentaire)
+    {
+        $user = auth()->user();
+        if (!$user->roles->contains('coach')) {
+            return redirect()->route('coach')->with('error', "Vous n'avez pas les accés pour supprimée les commentaires.");
+        }
+
+        $commentaire->delete();
+        return redirect()->route('coach')->with('success', 'Commentaire supprimée avec succès !');
+
+    }
+
+    public function editComment(Commentaire $commentaire)
+    {
+        $user = auth()->user();
+        if (!$user->roles->contains('coach')) {
+            return redirect()->route('coach')->with('error', "Vous n'avez pas les accés à la modification.");
+        }
+        return view('createcommentsUpdate', ['user' => $user, 'comment' => $commentaire]);
+
+    }
+
+    public function updateComment(CommentCreationRequest $request, Commentaire $commentaire)
+    {
+
+        $commentaire->title = $request->input('title');
+        $commentaire->description = $request->input('description');
+        $commentaire->save();
+
+        return redirect()->route('coach')->with('success', 'Commentaire édité avec succès !');
+    }
+
+
 }
